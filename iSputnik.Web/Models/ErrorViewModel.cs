ceebcaf29fcda7cfe6b9ac01757764f9108e1a4d
isputnikdotnet <sergeym@minskcity.com>
2018-07-31 19:43:26 -0500

    Add project files.

---
+++ iSputnik.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iSputnik.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
